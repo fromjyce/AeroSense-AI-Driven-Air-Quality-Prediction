7d60ee45fe1de837dbb339cdcf9bff7048a6db41
Jayashre <jaya2004kra@gmail.com>
2024-09-21 16:05:47 +0530

    modified

--- frontend/pages/About.js
+++ frontend/pages/About.js
@@ -1,34 +1,26 @@
-import Image from "next/image";
-
 export default function About() {
   return (
-    <section className="max-w-7xl mx-auto w-full bg-white shadow-lg p-6 flex flex-col md:flex-row rounded-xl">
-      <div className="md:w-1/2 flex flex-col justify-center p-4">
-      <h2 className="text-3xl font-bold">
+    <div className="about-section flex justify-between items-center gap-8 p-8">
+      <div className="text-content flex-1">
+        <h2 className="text-3xl font-bold">
           <span className="questrial">About</span>
           <span className="questrial title-card"> AeroSense</span>
         </h2>
-        <p className="text-lg mt-2 josefin_sans">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
+        <p className="text-lg josefin_sans leading-relaxed">
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        </p>
       </div>
-      <div className="md:w-1/2 flex justify-around items-center gap-x-3 p-4">
-        <Image
+      <div className="image-content flex flex-row gap-4 flex-1 items-end">
+        <img
           src="/side-photo-1.png"
-          alt="Image 1"
-          width={300}
-          height={200}
-          className="rounded"
+          alt="BuzzOn Earth"
+          className="w-full max-w-[300px]"
         />
-        <Image
+        <img
           src="/side-photo-2.png"
-          alt="Image 2"
-          width={300}
-          height={200}
-          className="rounded"
+          alt="Intel"
+          className="w-full max-w-[300px]"
         />
-        {" "}
       </div>
-    </section>
+    </div>
   );
 }

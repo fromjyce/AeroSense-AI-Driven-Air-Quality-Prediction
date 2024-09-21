237d50f637363211570257d1a79464b29a68d1a5
Jayashre <jaya2004kra@gmail.com>
2024-09-21 15:40:11 +0530

    added about fragment

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,8 +1,9 @@
-import Image from "next/image";
+import About from "@/pages/About";
 
 export default function Home() {
   return (
     <div>
+      <About />
       <h1 className="questrial">Hello</h1>
       <h1 className="dela_gothic_one">Hello</h1>
     </div>

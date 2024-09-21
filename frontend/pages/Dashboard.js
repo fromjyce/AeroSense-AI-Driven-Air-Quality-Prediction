a2d576feff4bb3853f2ff3d09e6decf22d2d50d3
Jayashre <jaya2004kra@gmail.com>
2024-09-22 00:21:26 +0530

    changed fonts

--- frontend/pages/Dashboard.js
+++ frontend/pages/Dashboard.js
@@ -53,7 +53,7 @@ export default function Dashboard() {
   return (
     <div className="dashboard-section flex justify-between gap-8 p-8">
       <div className="first-card w-[30%] bg-white p-4 shadow-lg flex flex-col items-start">
-        <h3 className="text-3xl font-bold text-left oswald">Predict AQI</h3>
+        <h3 className="text-3xl font-bold text-left oswald_card_title">Predict AQI</h3>
         <div>
           <div className="flex flex-row gap-4">
             <div className='flex flex-col items-start'>
@@ -121,7 +121,7 @@ export default function Dashboard() {
       </div>
 
       <div className="second-card w-[70%] bg-white p-4 shadow-lg flex flex-col items-start justify-center">
-        <h3 className="text-3xl font-bold text-left oswald">Current Weather Status</h3>
+        <h3 className="text-3xl font-bold text-left oswald_card_title">Current Weather Status</h3>
         <p className="text-lg mt-2 text-left josefin_sans">
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </p>

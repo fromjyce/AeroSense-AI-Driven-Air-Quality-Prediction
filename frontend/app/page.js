d45ea4e875e01d4eab115f7ee41a8b3dc4a2b207
Jayashre <jaya2004kra@gmail.com>
2024-09-21 17:30:34 +0530

    added dashboard

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,11 @@
 import About from "@/pages/About";
+import Dashboard from "@/pages/Dashboard";
 
 export default function Home() {
   return (
     <div>
       <About />
+      <Dashboard />
       <h1 className="questrial">Hello</h1>
       <h1 className="dela_gothic_one">Hello</h1>
     </div>

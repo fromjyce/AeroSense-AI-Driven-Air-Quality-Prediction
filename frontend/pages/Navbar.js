533b33c9bdaa6a7f57bf81d95c39b67442beedde
Jayashre <jaya2004kra@gmail.com>
2024-09-21 15:26:11 +0530

    added

--- frontend/pages/Navbar.js
+++ frontend/pages/Navbar.js
@@ -14,13 +14,13 @@ export default function Navbar() {
         <h1 className="text-3xl font-bold poppins dela_gothic_one">AeroSense</h1>
       </div>
       <nav className="flex space-x-6">
-        <Link href="#about" className="poppins text-lg px-4 py-2 hover:text-green hover:underline">
+        <Link href="#about" className="poppins font-bold text-lg px-4 py-2 hover:text-green hover:underline">
           About
         </Link>
-        <Link href="#predict" className="poppins text-lg px-4 py-2 hover:text-green hover:underline">
+        <Link href="#predict" className="poppins font-bold text-lg px-4 py-2 hover:text-green hover:underline">
           Predict
         </Link>
-        <Link href="#simulate" className="px-4 text-lg py-2 bg-green text-white rounded hover:bg-opacity-80">
+        <Link href="#simulate" className="px-4 font-bold text-lg py-2 bg-green text-white rounded hover:bg-opacity-80">
           Simulate
         </Link>
       </nav>

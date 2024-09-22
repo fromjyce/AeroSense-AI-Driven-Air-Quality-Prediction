3fd679ba6f2ea4c636670e8c9978faf1f9fa5771
Jayashre <jaya2004kra@gmail.com>
2024-09-22 20:40:18 +0530

    added link

--- frontend/pages/UpdateFooter.js
+++ frontend/pages/UpdateFooter.js
@@ -1,7 +1,9 @@
+import Link from "next/link";
+
 export default function UpdateFooter() {
     return (
       <div className="update-footer-section flex justify-center items-center p-4">
-        <h3 className="text-white questrial">Developed by Team <strong>Zoeo</strong> for BuzzOnEarth India Hackathon 2024, supported by <strong>IIT Kanpur</strong> and powered by <strong>Intel</strong>.</h3>
+        <h3 className="text-white questrial">Developed by Team <strong><Link href="#members">Zoeo</Link></strong> for BuzzOnEarth India Hackathon 2024, supported by <strong>IIT Kanpur</strong> and powered by <strong>Intel</strong>.</h3>
       </div>
     );
   }

2d36b31ba2fc9ffa3124db2def59c3d3dc34be3c
Jayashre <jaya2004kra@gmail.com>
2024-09-22 20:42:33 +0530

    added class name for link

--- frontend/pages/UpdateFooter.js
+++ frontend/pages/UpdateFooter.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function UpdateFooter() {
     return (
       <div className="update-footer-section flex justify-center items-center p-4">
-        <h3 className="text-white questrial">Developed by Team <strong><Link href="#members">Zoeo</Link></strong> for BuzzOnEarth India Hackathon 2024, supported by <strong>IIT Kanpur</strong> and powered by <strong>Intel</strong>.</h3>
+        <h3 className="text-white questrial">Developed by Team <strong><Link href="#members" className="team-link">Zoeo</Link></strong> for BuzzOnEarth India Hackathon 2024, supported by <strong>IIT Kanpur</strong> and powered by <strong>Intel</strong>.</h3>
       </div>
     );
   }

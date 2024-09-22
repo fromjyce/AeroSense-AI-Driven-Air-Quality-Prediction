98d7effc31ae1a38cc8e1ee66ff382cd97674037
Jayashre <jaya2004kra@gmail.com>
2024-09-22 20:30:34 +0530

    added update footer

---
+++ frontend/pages/UpdateFooter.js
@@ -0,0 +1,8 @@
+export default function UpdateFooter() {
+    return (
+      <div className="update-footer-section flex justify-center items-center p-4">
+        <h3 className="text-white questrial">Developed by Team <strong>Zoeo</strong> for BuzzOnEarth India Hackathon 2024, supported by <strong>IIT Kanpur</strong> and powered by <strong>Intel</strong>.</h3>
+      </div>
+    );
+  }
+  

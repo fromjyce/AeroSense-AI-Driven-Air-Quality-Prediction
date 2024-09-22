6be587c36b7f672f5ceb2d08ff39df36d6ccd2df
Jayashre <jaya2004kra@gmail.com>
2024-09-22 22:53:56 +0530

    added para content

--- frontend/pages/Simulator.js
+++ frontend/pages/Simulator.js
@@ -1,17 +1,23 @@
 export default function Simulator() {
-
     return (
       <>
         <h3 className="text-3xl font-bold mb-2 oswald_card_title">Air Pollution Simulator</h3>
-        <div className="iframe-container">
-        <iframe
-          className="responsive-iframe"
-          width="685px"
-          height="310px"
-          allowFullScreen={false}
-          src="https://lab.concord.org/embeddable.html#interactives/air-pollution/air-pollution-master.json"
-        ></iframe>
-      </div>    
+        <div className="flex flex-row">
+          <div className="paragraph-content w-1/2">
+            <p className="text-lg josefin_sans">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            </p>
+          </div>
+          <div className="iframe-container w-1/2">
+            <iframe
+              className="responsive-iframe"
+              width="100%"
+              height="300px"
+              allowFullScreen={false}
+              src="https://lab.concord.org/embeddable.html#interactives/air-pollution/air-pollution-master.json"
+            ></iframe>
+          </div>
+        </div>
       </>
     );
   }

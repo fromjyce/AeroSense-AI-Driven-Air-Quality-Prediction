8ca4295fa7b9716622265b7330affe94eb928a78
Jayashre <jaya2004kra@gmail.com>
2024-09-22 19:52:02 +0530

    added mid section

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,11 +1,13 @@
 import About from "@/pages/About";
 import Dashboard from "@/pages/Dashboard";
+import PastAQI from "@/pages/PastAQI";
 
 export default function Home() {
   return (
     <div>
       <About />
       <Dashboard />
+      <PastAQI />
       <h1 className="questrial">Hello</h1>
       <h1 className="dela_gothic_one">Hello</h1>
     </div>

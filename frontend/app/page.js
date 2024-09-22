a01cf32d701daff84c474c2ea28aafdb63680b8a
Jayashre <jaya2004kra@gmail.com>
2024-09-22 19:52:42 +0530

    added footer section

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,6 +1,7 @@
 import About from "@/pages/About";
 import Dashboard from "@/pages/Dashboard";
 import PastAQI from "@/pages/PastAQI";
+import Footer from "@/pages/Footer";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <About />
       <Dashboard />
       <PastAQI />
+      <Footer />
       <h1 className="questrial">Hello</h1>
       <h1 className="dela_gothic_one">Hello</h1>
     </div>

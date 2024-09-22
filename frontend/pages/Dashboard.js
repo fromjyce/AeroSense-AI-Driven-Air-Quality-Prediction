79b572ed5c20922861f0cb7b84795bf09b1a87e9
Jayashre <jaya2004kra@gmail.com>
2024-09-22 19:48:34 +0530

    mod

--- frontend/pages/Dashboard.js
+++ frontend/pages/Dashboard.js
@@ -5,11 +5,11 @@ export default function Dashboard() {
 
   return (
     <div className="dashboard-section flex justify-between gap-8 p-8">
-      <div id="predict" className="first-card w-[32%] bg-white p-4 shadow-lg flex flex-col items-start">
+      <div id="predict" className="first-card w-[32%] p-4 shadow-lg flex flex-col items-start">
         <PredictCard />
       </div>
 
-      <div className="second-card w-[68%] bg-white p-4 shadow-lg flex flex-col items-start justify-center">
+      <div className="second-card w-[68%] p-4 shadow-lg flex flex-col items-start justify-center">
        <CurrentWeather/>
       </div>
     </div>

7bf917f94ee10f41aaabf79d4e385c9ef06bfa1d
Jayashre <jaya2004kra@gmail.com>
2024-09-22 22:33:34 +0530

    added

--- frontend/pages/Simulator.js
+++ frontend/pages/Simulator.js
@@ -2,11 +2,17 @@ export default function Simulator() {
 
     return (
       <>
-        <h3 className="text-3xl font-bold mb-4 oswald_card_title">Past AQI Data</h3>
-          <p className="text-lg mt-2 text-left josefin_sans">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
-    </>
+        <h3 className="text-3xl font-bold mb-2 oswald_card_title">Air Pollution Simulator</h3>
+        <div className="iframe-container">
+        <iframe
+          className="responsive-iframe"
+          width="685px"
+          height="310px"
+          allowFullScreen={false}
+          src="https://lab.concord.org/embeddable.html#interactives/air-pollution/air-pollution-master.json"
+        ></iframe>
+      </div>    
+      </>
     );
   }
   

b5c06a48a41ca9e1e1144e3f7b2ed6fea7448c71
Jayashre <jaya2004kra@gmail.com>
2024-09-24 00:37:28 +0530

    Added read more

--- frontend/pages/About.js
+++ frontend/pages/About.js
@@ -4,10 +4,10 @@ export default function About() {
       <div className="text-content flex-1">
         <h2 className="text-3xl font-bold">
           <span className="questrial">About</span>
-          <span className="questrial title-card"> AeroSense</span>
+          <span className="questrial title-card"> AeroSense.</span>
         </h2>
         <p className="text-lg josefin_sans leading-relaxed">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.        </p>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Read <span><a href="#" className="more-details-about-product font-bold">More.</a></span></p>
       </div>
       <div className="image-content flex flex-row gap-4 flex-1 items-end">
         <img

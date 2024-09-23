391ad14e832ca4b7c0a7a097a193b3fb75094a48
Jayashre <jaya2004kra@gmail.com>
2024-09-23 18:01:49 +0530

    renamed

---
+++ frontend/pages/WeatherForecast.js
@@ -0,0 +1,20 @@
+import CurrentWeather from "./CurrentWeather";
+import WidgetContainer from "./WidgetContainer";
+
+
+export default function WeatherForecast() {
+
+  return (
+    <>
+      <h3 className="text-3xl font-bold text-left oswald_card_title">Weather Forecast</h3>
+      <div className="flex flex-col h-full">
+        <div className="flex justify-between p-4 border-b h-[35%] bg-yellow-300">
+          <CurrentWeather />
+        </div>
+        <div className="grid grid-cols gap-4 p-4 items-center flex-grow bg-yellow-300">
+          <WidgetContainer />
+        </div>
+      </div>
+    </>
+  );
+}

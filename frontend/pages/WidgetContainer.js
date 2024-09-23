5b1789fd8ef183428daded25bd04b1d6aedab0f9
Jayashre <jaya2004kra@gmail.com>
2024-09-23 18:57:14 +0530

    mod

--- frontend/pages/WidgetContainer.js
+++ frontend/pages/WidgetContainer.js
@@ -3,15 +3,13 @@ import WeatherWidget from "./WeatherWidget";
 export default function WidgetContainer() {
   return (
     <div className="flex flex-wrap justify-between items-start gap-4">
-      {/* Example Weather Widgets */}
-      <WeatherWidget date="12" maxTemp={25} minTemp={15} />
-      <WeatherWidget date="13" maxTemp={27} minTemp={17} />
-      <WeatherWidget date="14" maxTemp={30} minTemp={20} />
-      <WeatherWidget date="15" maxTemp={28} minTemp={18} />
-      <WeatherWidget date="16" maxTemp={26} minTemp={16} />
-      <WeatherWidget date="17" maxTemp={29} minTemp={19} />
-      <WeatherWidget date="18" maxTemp={29} minTemp={19} />
+      <WeatherWidget date="2024-09-12" maxTemp={25} minTemp={15} />
+      <WeatherWidget date="2024-09-13" maxTemp={27} minTemp={17} />
+      <WeatherWidget date="2024-09-14" maxTemp={30} minTemp={20} />
+      <WeatherWidget date="2024-09-15" maxTemp={28} minTemp={18} />
+      <WeatherWidget date="2024-09-16" maxTemp={26} minTemp={16} />
+      <WeatherWidget date="2024-09-17" maxTemp={29} minTemp={19} />
+      <WeatherWidget date="2024-09-18" maxTemp={29} minTemp={19} />
     </div>
   );
 }
-

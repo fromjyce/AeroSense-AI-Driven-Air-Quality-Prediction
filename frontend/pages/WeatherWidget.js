d83fc91c9dcb82878c1b870277e736f05d416baa
Jayashre <jaya2004kra@gmail.com>
2024-09-23 19:07:29 +0530

    added

--- frontend/pages/WeatherWidget.js
+++ frontend/pages/WeatherWidget.js
@@ -1,8 +1,14 @@
 export default function WeatherWidget({ date, maxTemp, minTemp }) {
+  const weatherDate = new Date(date);
+  const options = { weekday: 'short' };
+  const day = weatherDate.toLocaleDateString('en-US', options);
+  const dateNum = weatherDate.getDate();
+
   return (
-    <div className="flex flex-col items-center justify-center bg-white shadow-lg rounded-lg p-4 w-30 h-40">
-      <p className="text-3xl font-bold">{date}</p>
-      <p className="text-xl">
+    <div className="flex flex-col items-center justify-center bg-white shadow-lg rounded-lg w-32 h-40"> 
+      <p className="text-sm">{day}</p>
+      <p className="text-3xl font-bold">{dateNum}</p>
+      <p className="text-sm">
         {maxTemp}°<sup>C</sup> / {minTemp}°<sup>C</sup>
       </p>
     </div>

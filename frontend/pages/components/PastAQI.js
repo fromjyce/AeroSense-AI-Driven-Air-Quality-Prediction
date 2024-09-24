f5573f12547c038b8b751bd033a883b0ad4bcb90
Jayashre <jaya2004kra@gmail.com>
2024-09-24 15:10:24 +0530

    added pre final

--- frontend/pages/components/PastAQI.js
+++ frontend/pages/components/PastAQI.js
@@ -1,13 +1,64 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import CurrentAQIWidget from './widgets/CurrentAQIWidget';
+
 export default function PastAQI() {
-    return (
-      <div className="outer-container px-8">
-        <div className="past-aqi-section p-8 shadow-lg">
-          <h3 className="text-3xl font-bold mb-4 oswald_card_title">Past AQI Data</h3>
-          <p className="text-lg mt-2 text-left josefin_sans">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-          </p>
+  const [aqiData, setAqiData] = useState([]);
+
+  useEffect(() => {
+    const fetchAQIData = async () => {
+      try {
+        const AQI_FORECAST = "https://air-quality-api.open-meteo.com/v1/air-quality?latitude=13.0878&longitude=80.2785&hourly=us_aqi,pm2_5&timezone=auto&past_days=3&forecast_days=5";
+        const response = await fetch(AQI_FORECAST);
+        const data = await response.json();
+        const hourlyData = data.hourly;
+        const dailyData = {};
+
+        hourlyData.time.forEach((time, index) => {
+          const date = new Date(time).toISOString().split('T')[0];
+          if (!dailyData[date]) {
+            dailyData[date] = {
+              totalAQI: 0,
+              totalPM2_5: 0,
+              count: 0
+            };
+          }
+          dailyData[date].totalAQI += hourlyData.us_aqi[index];
+          dailyData[date].totalPM2_5 += hourlyData.pm2_5[index];
+          dailyData[date].count++;
+        });
+        const pastAQIData = Object.keys(dailyData).map(date => ({
+          date: date,
+          aqi: dailyData[date].totalAQI / dailyData[date].count,
+          pm2Concentration: dailyData[date].totalPM2_5 / dailyData[date].count
+        }));
+
+        setAqiData(pastAQIData);
+      } catch (error) {
+        console.error("Error fetching AQI data:", error);
+      }
+    };
+
+    fetchAQIData();
+  }, []);
+
+  return (
+    <div className="outer-container px-8">
+      <div className="past-aqi-section p-8 shadow-lg">
+        <h3 className="text-3xl font-bold mb-6 oswald_card_title text-center">Air Quality Index Forecast</h3>
+        <div className="aqi-widget-container flex flex-wrap gap-5 mt-4 items-center justify-center">
+          {aqiData.map((item, index) => (
+            <CurrentAQIWidget 
+              key={index}
+              date={item.date}
+              aqi={item.aqi.toFixed(1)}
+              pm2Concentration={item.pm2Concentration.toFixed(1)}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
+

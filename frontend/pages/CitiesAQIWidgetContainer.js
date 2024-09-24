1382c5c62e1858a2fc6cc0930cd5cd96a1b323f7
Jayashre <jaya2004kra@gmail.com>
2024-09-24 12:00:16 +0530

    added

--- frontend/pages/CitiesAQIWidgetContainer.js
+++ frontend/pages/CitiesAQIWidgetContainer.js
@@ -1,10 +1,16 @@
+import IndiaAQIDisplay from "./IndiaAQIDisplay";
+import MetroAQIWidgetContainer from "./MetroAQIWidgetContainer";
+
 export default function CitiesAQIWidgetContainer() {
   return (
-    <>
-    <h3 className="text-3xl font-bold mb-4 oswald_card_title">Metro Cities - Air Quality Index</h3>
-        <p className="text-lg mt-2 text-left josefin_sans">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-    </>
+    <div className="flex flex-col h-full">
+      <div className="first-half flex-grow-0">
+        <h3 className="text-3xl font-bold mb-4 oswald_card_title">Metro Cities - Air Quality Index</h3>
+        <IndiaAQIDisplay />
+      </div>
+      <div className="second-half flex-grow">
+        <MetroAQIWidgetContainer />
+      </div>
+    </div>
   );
 }

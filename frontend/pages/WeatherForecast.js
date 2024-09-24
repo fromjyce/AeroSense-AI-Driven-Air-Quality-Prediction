9c4b4998d9dcbe6212b110470ba329c6f17364fb
Jayashre <jaya2004kra@gmail.com>
2024-09-24 12:33:58 +0530

    mod

--- frontend/pages/WeatherForecast.js
+++ frontend/pages/WeatherForecast.js
@@ -8,10 +8,10 @@ export default function WeatherForecast() {
     <>
       <h3 className="text-3xl font-bold text-left oswald_card_title">Weather Forecast</h3>
       <div className="flex flex-col h-full">
-        <div className="flex justify-between p-4 border-b h-[35%]">
+        <div className="flex justify-between border-b h-[35%]">
           <CurrentWeather />
         </div>
-        <div className="grid grid-cols gap-4 p-4 items-center flex-grow">
+        <div className="grid grid-cols gap-4 items-center flex-grow">
           <WidgetContainer />
         </div>
       </div>

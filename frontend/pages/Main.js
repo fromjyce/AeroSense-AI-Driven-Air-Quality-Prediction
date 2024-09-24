e12c5336e4f8367921cb776844f34eeaf157e99d
Jayashre <jaya2004kra@gmail.com>
2024-09-24 13:44:09 +0530

    3

---
+++ frontend/pages/Main.js
@@ -0,0 +1,15 @@
+import About from "@/pages/About";
+import Dashboard from "@/pages/Dashboard";
+import PastAQI from "@/pages/PastAQI";
+import Footer from "@/pages/Footer";
+
+export default function Main() {
+  return (
+    <div>
+      <About />
+      <Dashboard />
+      <PastAQI />
+      <Footer />
+    </div>
+  );
+}

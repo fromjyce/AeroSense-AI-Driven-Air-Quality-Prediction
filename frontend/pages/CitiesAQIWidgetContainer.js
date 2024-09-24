d1b94a3ca94916ab1f707fb75367b59e4fe558e0
Jayashre <jaya2004kra@gmail.com>
2024-09-24 09:35:04 +0530

    added initial cities container

---
+++ frontend/pages/CitiesAQIWidgetContainer.js
@@ -0,0 +1,10 @@
+export default function CitiesAQIWidgetContainer() {
+  return (
+    <>
+    <h3 className="text-3xl font-bold mb-4 oswald_card_title">Metro Cities - Air Quality Index</h3>
+        <p className="text-lg mt-2 text-left josefin_sans">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+    </>
+  );
+}

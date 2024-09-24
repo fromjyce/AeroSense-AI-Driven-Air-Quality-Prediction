bdc5da96a056f3a29d29f757b7556bcdfe567b86
Jayashre <jaya2004kra@gmail.com>
2024-09-24 15:28:37 +0530

    added color logic

--- frontend/pages/components/widgets/CurrentAQIWidget.js
+++ frontend/pages/components/widgets/CurrentAQIWidget.js
@@ -1,16 +1,34 @@
 import React from 'react';
 
+const getAqiInfo = (aqi) => {
+  if (aqi <= 50) return { color: "#00b050" };
+  if (aqi <= 100) return { color: "#92d050" };
+  if (aqi <= 200) return { color: "#ffff00" };
+  if (aqi <= 300) return { color: "#ffc000" };
+  if (aqi <= 400) return { color: "#f60000" };
+  return { color: "#c00000" };
+};
+
 export default function CurrentAQIWidget({ date, aqi, pm2Concentration }) {
   const day = new Date(date).toLocaleDateString('en-US', { weekday: 'short' });
   const numericDay = new Date(date).getDate();
   const month = new Date(date).toLocaleDateString('en-US', { month: 'short' });
+  const { color } = getAqiInfo(aqi);
+  const currentDate = new Date().toISOString().split('T')[0];
+  const widgetDate = new Date(date).toISOString().split('T')[0];
+  const backgroundColor = widgetDate === currentDate ? "#F9F6EE" : "#ffffff";
 
   return (
-    <div className="current-aqi-widget bg-gray-100 shadow-lg rounded-lg p-4 flex flex-col items-center justify-center text-center">
+    <div
+      className="current-aqi-widget shadow-lg rounded-lg p-4 flex flex-col items-center justify-center text-center"
+      style={{ backgroundColor: backgroundColor }}
+    >
       <p className="font-bold current-date poppins">
-        {day}, {month} {numericDay} 
+        {day}, {month} {numericDay}
       </p>
-      <p className="text-6xl font-bold mt-2 bebas_neue">{aqi}</p>
+      <p className="text-6xl font-bold mt-2 bebas_neue" style={{ color: color }}>
+        {aqi}
+      </p>
       <p className="current-concentration josefin_sans mt-2">PM2.5: {pm2Concentration}</p>
     </div>
   );

153716d4aa88f297ee9efd99865dba26422ea92c
Jayashre <jaya2004kra@gmail.com>
2024-09-27 14:02:24 +0530

    added city heading

--- frontend/pages/components/elements/CitiesAQIWidgetContainer.js
+++ frontend/pages/components/elements/CitiesAQIWidgetContainer.js
@@ -3,12 +3,11 @@ import MetroAQIWidgetContainer from "./MetroAQIWidgetContainer";
 
 export default function CitiesAQIWidgetContainer() {
   return (
-    <div className="flex flex-col h-full">
-      <div className="first-half flex-grow-0">
-        <h3 className="text-3xl font-bold mb-4 oswald_card_title">Metro Cities - Air Quality Index</h3>
+    <div className="flex flex-row h-full">
+      <div className="first-half flex-shrink-0">
         <IndiaAQIDisplay />
       </div>
-      <div className="second-half flex-grow mt-4">
+      <div className="second-half flex-grow ml-3">
         <MetroAQIWidgetContainer />
       </div>
     </div>

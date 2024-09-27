877ad3545960cc80a1a251d403544be922d7613a
Jayashre <jaya2004kra@gmail.com>
2024-09-27 23:05:49 +0530

    aligned center

--- frontend/pages/components/elements/CitiesAQIWidgetContainer.js
+++ frontend/pages/components/elements/CitiesAQIWidgetContainer.js
@@ -3,7 +3,7 @@ import MetroAQIWidgetContainer from "./MetroAQIWidgetContainer";
 
 export default function CitiesAQIWidgetContainer() {
   return (
-    <div className="flex flex-row h-full">
+    <div className="flex flex-row h-full align-items items-center">
       <div className="first-half flex-shrink-0">
         <IndiaAQIDisplay />
       </div>

a091a95a9d2f2a928dc03949ab732e78fb9dc811
Jayashre <jaya2004kra@gmail.com>
2024-09-28 11:21:03 +0530

    #

--- frontend/pages/components/elements/InfoDisplay.js
+++ frontend/pages/components/elements/InfoDisplay.js
@@ -6,9 +6,8 @@ export default function InfoDisplay({formattedDate}) {
 
   return (
     <>
-    <h1 className="text-lg font-semibold">Information</h1>
-        <p className="text-lg text-gray-700">{formattedDate}</p>
-        <p className="text-lg text-gray-700">City: Chennai, Tamil Nadu</p>
+        <p className="text-2xl">{formattedDate}</p>
+        <p className="text-2xl">City: Chennai, Tamil Nadu</p>
     </>
   );
 }

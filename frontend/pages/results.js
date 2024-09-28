d2f9ae4261cb0fa999671f956d2bcefc1988be77
Jayashre <jaya2004kra@gmail.com>
2024-09-28 09:26:24 +0530

    added results page

--- frontend/pages/results.js
+++ frontend/pages/results.js
@@ -2,6 +2,7 @@
 
 import { useSearchParams } from 'next/navigation';
 import MapContainer from './components/MapContainer';
+import InformationBoard from './components/InformationBoard';
 import "../app/globals.css";
 
 export default function Results() {
@@ -15,24 +16,14 @@ export default function Results() {
 
   return (
       <div className="min-h-screen result-background">
-        <div className="text-center py-6">
-          <h1 className="text-3xl font-bold">Predictions</h1>
-        </div>
-
+          <h1 className="text-3xl font-bold text-center py-6 predict-title">Predictions</h1>
         <div className="flex flex-row justify-between p-5">
           <div className="w-1/3 mr-5 border border-gray-300 rounded-lg shadow-lg p-4 bg-white">
           <MapContainer markers={markers} />
           </div>
-          <div className="w-2/3">
-            <div className="border border-gray-300 rounded-lg shadow-lg p-4 bg-white">
-              <h2 className="text-xl font-semibold mb-2">Additional Information</h2>
-              {message ? (
-                <p className="text-lg text-gray-700">{message}</p>
-              ) : (
-                <p className="text-lg text-gray-500">No message available.</p>
-              )}
-              <p className="text-lg text-gray-700">Display other relevant results or details here.</p>
-            </div>
+          <div className="w-2/3 border border-gray-300 rounded-lg shadow-lg p-4 bg-white">
+              <h2 className="text-xl font-semibold mb-2 text-center">Additional Information</h2>
+              <InformationBoard />
           </div>
         </div>
       </div>

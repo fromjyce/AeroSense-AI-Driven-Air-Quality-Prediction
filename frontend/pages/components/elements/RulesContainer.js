313002638f5f1d6d08146ea362899f2ecc7c1d91
Jayashre <jaya2004kra@gmail.com>
2024-09-28 11:29:33 +0530

    added rules img

--- frontend/pages/components/elements/RulesContainer.js
+++ frontend/pages/components/elements/RulesContainer.js
@@ -3,11 +3,13 @@
 import "../../../app/globals.css";
 
 export default function RulesContainer() {
-
   return (
-    <>
-               <h2 className="text-lg font-semibold">Additional Information</h2>
-               <p className="text-lg text-gray-700">You can display more details or statistics here.</p>
-    </>
+    <div className="flex flex-col items-center ml-4">
+      <img 
+        src="/misc/cpcb_rules.png"
+        alt="CPCB Rules" 
+        className="w-auto h-auto rounded shadow-lg"
+      />
+    </div>
   );
 }

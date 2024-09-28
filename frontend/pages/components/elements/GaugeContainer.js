a6c503042db9c305dd329f44622bad30e483d090
Jayashre <jaya2004kra@gmail.com>
2024-09-28 11:20:37 +0530

    added cols

--- frontend/pages/components/elements/GaugeContainer.js
+++ frontend/pages/components/elements/GaugeContainer.js
@@ -5,36 +5,44 @@ import dynamic from "next/dynamic";
 const GaugeComponent = dynamic(() => import('react-gauge-component'), { ssr: false });
 
 export default function GaugeContainer() {
-
   return (
     <>
-                <GaugeComponent
-            arc={{
-                subArcs: [
-                {
-                    limit: 20,
-                    color: '#EA4228',
-                    showTick: true
-                },
-                {
-                    limit: 40,
-                    color: '#F58B19',
-                    showTick: true
-                },
-                {
-                    limit: 60,
-                    color: '#F5CD19',
-                    showTick: true
-                },
-                {
-                    limit: 100,
-                    color: '#5BE12C',
-                    showTick: true
-                },
-                ]
-            }}
-            value={50}
-            />
+      <GaugeComponent
+        value={50.5}
+        type="radial"
+        minValue={0}
+        maxValue={400}
+        labels={{
+          tickLabels: {
+            type: "inner",
+            ticks: [
+              { value: 0 },
+              { value: 50 },
+              { value: 100 },
+              { value: 200 },
+              { value: 300 },
+              { value: 400 }
+            ]
+          }
+        }}
+        arc={{
+          colorArray: ['#00b050', '#92d050', '#ff8000', '#ffc000'],
+          subArcs: [
+            { limit: 50 },   
+            { limit: 100 },  
+            { limit: 200 },  
+            { limit: 300 }, 
+            { limit: 400 }
+          ],
+          padding: 0.02,
+          width: 0.3
+        }}
+        pointer={{
+          elastic: true,
+          animationDelay: 0,
+        }}
+        className="gauge-text"
+      />
     </>
   );
 }

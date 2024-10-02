8278468ba5a576991c638c239cc4eedf6b658f64
Jayashre <jaya2004kra@gmail.com>
2024-10-02 12:55:24 +0530

    added h3

--- frontend/pages/components/elements/CitiesAQIWidgetContainer.js
+++ frontend/pages/components/elements/CitiesAQIWidgetContainer.js
@@ -3,6 +3,9 @@ import MetroAQIWidgetContainer from "./MetroAQIWidgetContainer";
 
 export default function CitiesAQIWidgetContainer() {
   return (
+    <><h3 className="text-3xl md:text-3xl lg:text-3xl font-bold mb-4 oswald_card_title">
+    Metro Cities - Air Quality Index
+  </h3>
     <div className="flex flex-row h-full align-items items-center">
       <div className="first-half flex-shrink-0">
         <IndiaAQIDisplay />
@@ -11,5 +14,6 @@ export default function CitiesAQIWidgetContainer() {
         <MetroAQIWidgetContainer />
       </div>
     </div>
+    </>
   );
 }

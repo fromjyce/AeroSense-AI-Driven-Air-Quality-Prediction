f6b0b18cd62dab9169df115a8d61f17bd6e6432c
Jayashre <jaya2004kra@gmail.com>
2024-10-04 08:20:59 +0530

    mod

--- frontend/pages/components/elements/AISuggestionsContainer.js
+++ frontend/pages/components/elements/AISuggestionsContainer.js
@@ -4,7 +4,7 @@ import "../../../app/globals.css";
 
 export default function AISuggestionsContainer() {
   return (
-    <div className="p-5 border border-gray-300 rounded-lg shadow-lg bg-white">
+    <div className="rounded-lg shadow-lg bg-white ai-suggs p-4">
       <h2 className="text-lg font-semibold">AI Suggestions</h2>
       <p className="text-lg text-gray-700">Chennai has recorded an Air Quality Index (AQI) of 50.0, indicating "Good" air quality with little risk to residents. As a Pollution Control Manager, it is essential to maintain this status by promoting public transportation and enhancing green spaces. Encouraging electric vehicle usage and improving dust control measures at construction sites can further reduce pollution. Public awareness campaigns about eco-friendly practices will engage citizens in maintaining air quality. Lastly, establishing more air quality monitoring stations will help identify pollution sources in real-time.</p>
     </div>

8681aaf68e716cc072ea8b5817929b90e7fee718
Jayashre <jaya2004kra@gmail.com>
2024-10-04 23:12:57 +0530

    rem colors

--- frontend/pages/components/ResultCharts.js
+++ frontend/pages/components/ResultCharts.js
@@ -4,7 +4,7 @@ import BarPieChartContainer from "./elements/BarPieChartContainer";
 export default function ResultCharts() {
     return (
       <div className="flex h-full gap-x-4">
-        <div className="w-[40%] bg-green-500 h-full">
+        <div className="w-[40%] h-full">
           <InfoGaugeContainer />
         </div>
         <div className="w-[60%] bg-orange-500 h-full">

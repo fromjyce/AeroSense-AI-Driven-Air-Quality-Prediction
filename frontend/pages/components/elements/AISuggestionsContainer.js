f6929c434ec357e34a0c1a66f11a298ab7c3d77c
Jayashre <jaya2004kra@gmail.com>
2024-10-04 20:53:39 +0530

    added styles

--- frontend/pages/components/elements/AISuggestionsContainer.js
+++ frontend/pages/components/elements/AISuggestionsContainer.js
@@ -1,12 +1,29 @@
-'use client';
-
-import "../../../app/globals.css";
+import TipsAndUpdatesTwoToneIcon from '@mui/icons-material/TipsAndUpdatesTwoTone';
+import Typewriter from 'typewriter-effect';
 
 export default function AISuggestionsContainer() {
+  const suggestion = `Chennai has recorded an Air Quality Index (AQI) of 50.0, indicating "Good" air quality with little risk to residents. As a Pollution Control Manager, it is essential to maintain this status by promoting public transportation and enhancing green spaces. Encouraging electric vehicle usage and improving dust control measures at construction sites can further reduce pollution. Public awareness campaigns about eco-friendly practices will engage citizens in maintaining air quality. Lastly, establishing more air quality monitoring stations will help identify pollution sources in real-time.`;
+
   return (
     <div className="rounded-lg shadow-lg bg-white ai-suggs p-4">
-      <h2 className="text-lg font-semibold">AI Suggestions</h2>
-      <p className="text-lg text-gray-700">Chennai has recorded an Air Quality Index (AQI) of 50.0, indicating "Good" air quality with little risk to residents. As a Pollution Control Manager, it is essential to maintain this status by promoting public transportation and enhancing green spaces. Encouraging electric vehicle usage and improving dust control measures at construction sites can further reduce pollution. Public awareness campaigns about eco-friendly practices will engage citizens in maintaining air quality. Lastly, establishing more air quality monitoring stations will help identify pollution sources in real-time.</p>
+      <div className='flex flex-row gap-2'>
+      <TipsAndUpdatesTwoToneIcon sx={{ color: '#638763'}}/>
+      <h2 className="text-xl font-semibold questrial title-sugg">Breathe Smarter with AI</h2>
+      </div>
+      <div className="text-lg questrial font-bold">
+      <Typewriter
+        onInit={(typewriter) => {
+        typewriter
+        .typeString(suggestion)
+        .pauseFor(5000)
+        .start();
+          }}
+          options={{
+            delay: 75,
+          }}
+          />
+      </div>
+      <div><h2 className='text-right power-tag text-lg josefin_sans title-sugg'>Powered by OpenVINO™</h2></div>
     </div>
   );
 }

e3a564045213a172870846c2ccb62f1a337f76de
Jayashre <jaya2004kra@gmail.com>
2024-10-04 08:35:22 +0530

    added map compontent

---
+++ frontend/pages/components/MapSuggRules.js
@@ -0,0 +1,26 @@
+import AISuggestionsContainer from "./elements/AISuggestionsContainer";
+import MapContainer from "./MapContainer";
+
+const markers = [
+  { lat: 13.0843, lng: 80.2705, title: 'Chennai' },
+];
+
+export default function MapSuggRules() {
+  return (
+    <div className="flex flex-col md:flex-row justify-between gap-4 h-full">
+      <div className="w-full md:w-[25%] flex flex-col">
+      <MapContainer markers={markers} />
+      </div>
+      <div className="w-full md:w-[50%] flex flex-col">
+        <AISuggestionsContainer />
+      </div>
+      <div className="w-full md:w-[25%] flex flex-col justify-center">
+        <img
+          src="/misc/cpcb_rules.png"
+          alt="CBCP Rules"
+          className="rounded-lg rules-image shadow-lg"
+        />
+      </div>
+    </div>
+  );
+}

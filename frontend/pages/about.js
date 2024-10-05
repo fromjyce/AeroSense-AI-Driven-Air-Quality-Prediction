a591f5b21e1fdfa5ec12a2bec4c59a53af91fe99
Jayashre <jaya2004kra@gmail.com>
2024-10-05 16:23:23 +0530

    added title

--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,9 +1,17 @@
+import Head from "next/head";
+
 export default function About() {
     return (
+      <>
+      <Head>
+      <title>Aerosense - About</title>
+      <meta name="description" content="About AeroSense" />
+      </Head>
       <div>
         <h1>About Page</h1>
         <p>This is the about page content.</p>
       </div>
+      </>
     );
   }
   

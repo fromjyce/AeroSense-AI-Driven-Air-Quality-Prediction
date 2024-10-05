7bda36b770de144f17d085ccfbd1b413652c5de6
Jayashre <jaya2004kra@gmail.com>
2024-10-05 23:41:33 +0530

    added bar

--- frontend/pages/components/elements/results/BarPieChartContainer.js
+++ frontend/pages/components/elements/results/BarPieChartContainer.js
@@ -1,7 +1,20 @@
+import BarChartContainer from "../../widgets/BarChartContainer";
+
 export default function BarPieChartContainer() {
-    return (
-      <>
-      HI</>
-    );
-  }
-  
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold oswald gauge-project-text mb-8">
+        Air Quality Pollutant Analysis
+      </h2>
+      <div className="flex">
+        <div className="w-2/3">
+          <BarChartContainer />
+        </div>
+        <div className="w-1/3 pollutant-paragraph">
+          <p className="poppins">
+          Monitoring key air pollutants like PM2.5, PM10, NO, NO2, NOx, CO, SO2, O3, and Benzene is essential for assessing air quality and its health impacts. Understanding these concentrations allows us to make informed decisions and promote cleaner air for everyone. Stay aware of these levels to mitigate their effects and ensure a healthier environment.          </p>
+        </div>
+      </div>
+    </div>
+  );
+}

8d9a54b7681582527ec0ddb5b93309e3e191e5ba
Jayashre <jaya2004kra@gmail.com>
2024-10-05 22:53:22 +0530

    Added bg color

--- frontend/pages/components/results/ResultCharts.js
+++ frontend/pages/components/results/ResultCharts.js
@@ -4,10 +4,10 @@ import BarPieChartContainer from "../elements/results/BarPieChartContainer";
 export default function ResultCharts() {
     return (
       <div className="flex h-full gap-x-4">
-        <div className="w-[40%] h-full">
+        <div className="w-[40%]">
           <InfoGaugeContainer />
         </div>
-        <div className="w-[60%] bg-orange-500 h-full">
+        <div className="w-[60%] rounded-lg shadow-lg bg-white p-4">
           <BarPieChartContainer />
         </div>
       </div>

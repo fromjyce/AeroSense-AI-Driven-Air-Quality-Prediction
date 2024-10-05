cfe449963393ccb3168f302f96fc1c183f5d1451
Jayashre <jaya2004kra@gmail.com>
2024-10-05 13:31:39 +0530

    added info display

--- frontend/pages/components/elements/InfoGaugeContainer.js
+++ frontend/pages/components/elements/InfoGaugeContainer.js
@@ -1,18 +1,36 @@
 import GaugeContainer from "./GaugeContainer";
+import InfoDisplay from "./InfoDisplay";
 
 export default function InfoGaugeContainer() {
   const value = 150;
-    return (
-      <div className="flex flex-col h-full gap-y-3">
-        <div className="bg-orange-500">
-        You can achieve the layout with two vertically stacked divs by using Tailwind's flex and height utilities. Here's how you can structure it with a gap between the top and bottom sections
-        </div>
-        <div className="rounded-lg shadow-lg bg-white">
-            <div className="gauge-main-container p-4">
-          <GaugeContainer value={value}/>
-          </div>
+  const date ="";
+  const datetime = "";
+  const city = "";
+  const stationName = "Tirupati - APPCB, Andhra Pradesh";
+  const startDate = "";
+  const endDate = "";
+  const startDatetime = "2024-10-10T18:00:00";
+  const endDatetime = "2024-10-10T18:00:00";
+
+  return (
+    <div className="flex flex-col h-full gap-y-4">
+      <div className="rounded-lg shadow-lg bg-white">
+        <InfoDisplay 
+          date={date} 
+          datetime={datetime} 
+          city={city} 
+          stationName={stationName} 
+          startDate={startDate} 
+          endDate={endDate} 
+          startDatetime={startDatetime} 
+          endDatetime={endDatetime} 
+        />
+      </div>
+      <div className="rounded-lg shadow-lg bg-white">
+        <div className="gauge-main-container p-4">
+          <GaugeContainer value={value} />
         </div>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}

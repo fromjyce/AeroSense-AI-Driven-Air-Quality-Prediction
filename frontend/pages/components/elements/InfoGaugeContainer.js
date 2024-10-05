785a3713519859870086c6ca8e4b4f1d69ebd172
Jayashre <jaya2004kra@gmail.com>
2024-10-05 11:23:49 +0530

    added gauge

--- frontend/pages/components/elements/InfoGaugeContainer.js
+++ frontend/pages/components/elements/InfoGaugeContainer.js
@@ -1,7 +1,18 @@
+import GaugeContainer from "./GaugeContainer";
+
 export default function InfoGaugeContainer() {
+  const value = 150;
     return (
-      <>
-      HI</>
+      <div className="flex flex-col h-full gap-y-3">
+        <div className="bg-orange-500">
+        You can achieve the layout with two vertically stacked divs by using Tailwind's flex and height utilities. Here's how you can structure it with a gap between the top and bottom sections
+        </div>
+        <div className="rounded-lg shadow-lg bg-white">
+            <div className="gauge-main-container p-4">
+          <GaugeContainer value={value}/>
+          </div>
+        </div>
+      </div>
     );
   }
   

d502ec14be212ef1d7ca3d8e5624e0d2d1e73e15
Jayashre <jaya2004kra@gmail.com>
2024-10-05 13:49:09 +0530

    renamed

--- frontend/pages/components/elements/main/MetroAQIWidgetContainer.js
+++ frontend/pages/components/elements/main/MetroAQIWidgetContainer.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from 'react';
-import CityAQIWidget from '../widgets/CityAQIWidget';
+import CityAQIWidget from '../../widgets/CityAQIWidget';
 
 export default function MetroAQIWidgetContainer() {
   const [metroCitiesData, setMetroCitiesData] = useState([]);

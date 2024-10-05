bd987038ff8671f68e37b87e19bb3fee287ac8ba
Jayashre <jaya2004kra@gmail.com>
2024-10-05 22:52:31 +0530

    Added title and meta

--- frontend/pages/Main.js
+++ frontend/pages/Main.js
@@ -1,15 +1,22 @@
 import About from "@/pages/components/main/About";
+import Head from "next/head";
 import Dashboard from "@/pages/components/main/Dashboard";
 import PastAQI from "@/pages/components/main/PastAQI";
 import Footer from "@/pages/components/main/Footer";
 
 export default function Main() {
   return (
+    <>
+    <Head>
+        <title>Aerosense</title>
+        <meta name="description" content="An AI-driven air quality monitoring and prediction system." />
+      </Head>
     <div>
       <About />
       <Dashboard />
       <PastAQI />
       <Footer />
     </div>
+    </>
   );
 }

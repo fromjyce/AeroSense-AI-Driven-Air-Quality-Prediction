2d2e0a693ff4b7d5f2b7597be5f525fcb1106a40
Jayashre <jaya2004kra@gmail.com>
2024-10-06 23:16:48 +0530

    added parameters

--- frontend/pages/components/results/ResultCharts.js
+++ frontend/pages/components/results/ResultCharts.js
@@ -1,11 +1,20 @@
 import InfoGaugeContainer from "../elements/results/InfoGaugeContainer";
 import BarPieChartContainer from "../elements/results/BarPieChartContainer";
 
-export default function ResultCharts() {
+export default function ResultCharts({ stationId, city, datetime, date, startDatetime, endDatetime, startDate, endDate }) {
     return (
       <div className="flex h-full gap-x-4">
         <div className="w-[40%]">
-          <InfoGaugeContainer />
+        <InfoGaugeContainer 
+          stationId={stationId}
+          city={city}
+          datetime={datetime}
+          date={date}
+          startDatetime={startDatetime}
+          endDatetime={endDatetime}
+          startDate={startDate}
+          endDate={endDate}
+        />
         </div>
         <div className="w-[60%] rounded-lg shadow-lg bg-white p-4">
           <BarPieChartContainer />

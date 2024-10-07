12ff681d836a1b062e0a5efbe1c676e61379fd9b
Jayashre <jaya2004kra@gmail.com>
2024-10-07 21:13:06 +0530

    added images

--- frontend/pages/components/elements/results/AISuggestionsContainer.js
+++ frontend/pages/components/elements/results/AISuggestionsContainer.js
@@ -1,27 +1,41 @@
 import TipsAndUpdatesTwoToneIcon from '@mui/icons-material/TipsAndUpdatesTwoTone';
 import Typewriter from 'typewriter-effect';
 
-export default function AISuggestionsContainer() {
-  const suggestion = `Chennai has recorded an Air Quality Index (AQI) of 50.0, indicating "Good" air quality with little risk to residents. As a Pollution Control Manager, it is essential to maintain this status by promoting public transportation and enhancing green spaces. Encouraging electric vehicle usage and improving dust control measures at construction sites can further reduce pollution. Public awareness campaigns about eco-friendly practices will engage citizens in maintaining air quality. Lastly, establishing more air quality monitoring stations will help identify pollution sources in real-time.`;
+export default function AISuggestionsContainer({message}) {
+  const suggestion = message;
 
   return (
     <div className="rounded-lg shadow-lg bg-white ai-suggs p-4">
       <div className='flex flex-row gap-2'>
-      <TipsAndUpdatesTwoToneIcon sx={{ color: '#638763'}}/>
-      <h2 className="text-xl font-black questrial title-sugg">Breathe Smarter with AI</h2>
+        <TipsAndUpdatesTwoToneIcon sx={{ color: '#638763'}} />
+        <h2 className="text-xl font-black questrial title-sugg">Breathe Smarter with AI</h2>
       </div>
-      <div className="text-lg questrial font-semibold">
-      <Typewriter
-        onInit={(typewriter) => {
-        typewriter
-        .typeString(suggestion)
-        .pauseFor(5000)
-        .start();
-          }}
-          options={{
-            delay: 80,
-          }}
+      <div className="flex flex-row justify-between items-center mt-4">
+        <div className="w-2/3">
+          <div className="text-lg questrial font-semibold">
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter
+                  .typeString(suggestion)
+                  .pauseFor(5000)
+                  .start();
+              }}
+              options={{
+                delay: 80,
+              }}
+            />
+          </div>
+        </div>
+
+        
+        <div className="w-1/3 flex justify-end">
+          <img
+            src="/misc/warning.jpeg"
+            alt="Air Quality Monitoring"
+            className="rounded-lg shadow-lg"
+            style={{ maxHeight: '150px', maxWidth: '100%' }}
           />
+        </div>
       </div>
       <div><h2 className='text-right power-tag text-lg josefin_sans title-sugg'>Powered by OpenVINO™</h2></div>
     </div>
